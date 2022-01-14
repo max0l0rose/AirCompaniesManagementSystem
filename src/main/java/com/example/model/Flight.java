81970dc7cb0a6731976c24a2717f5f0adbfeef90
9956799@gmail.com <Qqq_12345>
2022-01-14 21:29:57 +0200

    Db airplane company flight sql script

--- src/main/java/com/example/model/Flight.java
+++ src/main/java/com/example/model/Flight.java
@@ -17,24 +17,36 @@ import java.time.OffsetDateTime;
 public class Flight extends BaseEntity {
 
 	//@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	FlightStatus status;
 
 	@ManyToOne
+	// I think a flight may be not assigned to a company - nullable
 	private Company company;
 
+	// I think a flight may be not assigned to a plane
+	// nullable
 	@ManyToOne(cascade = CascadeType.ALL)
 	private Airplane airplane;
 
+	@Column(nullable = false)
 	private String departCountry;
 
+	@Column(nullable = false)
 	private String destCountry;
 
 	private int distance;
 
+	@Column(nullable = false)
 	private LocalTime estimatedFlightTime;
 
+	@Column(columnDefinition="DATETIME(0)")
 	private OffsetDateTime startedDateTime;
+
+	@Column(columnDefinition="DATETIME(0)")
 	private OffsetDateTime endedDateTime;
+
+	@Column(columnDefinition="DATETIME(0)")
 	private OffsetDateTime delayStartedDateTime;
 
 	@Column(columnDefinition="DATETIME(0)")
